f4957514cc20183e53ddaa9f8276058592976261
mtaromi <mtaromirad@gmail.com>
2024-04-12 11:41:08 +0200

    Fix the build

--- Hash.cs
+++ Hash.cs
@@ -8,6 +8,7 @@
         /// This is how VB Anonymous Types combine hash values for fields.
         /// </summary>
         internal static int Combine(int newKey, int currentKey)
+        {
             return unchecked((currentKey * (int)0xA5555529) + newKey);
         }
 }
